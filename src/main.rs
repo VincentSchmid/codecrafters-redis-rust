ceba730d0892dbd6a28cfc1f5b7fdb07e2fde056
Vincent Schmid <Vincent.Schmid@ti8m.ch>
2023-02-27 23:48:44 +0100

    implementing PING

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+mod Response;
+
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
+use crate::Response::RespDataType;
+
+
+fn handle_client(mut stream: TcpStream) {
+    // if PING, return PONG
+    let mut buf = [0; 512];
+    let command = stream.read(&mut buf).unwrap();
+    println!("command: {}", command);
+    let response = Response::RespSimpleString { content: "PONG".to_string() };
+    stream.write(response.get_response().as_bytes()).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,8 +24,8 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
